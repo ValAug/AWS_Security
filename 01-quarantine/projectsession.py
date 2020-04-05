19d84d393892d78411bf7fc869a19536a3acef9d
ValAug <valdiviaaugusto9@gmail.com>
2020-04-04 20:45:33 -0400

    projectsession.py updated it

--- 01-quarantine/projectsession.py
+++ 01-quarantine/projectsession.py
@@ -1,15 +1,11 @@
 # coding: utf-8
 import botostubs
 import boto3
-<<<<<<< HEAD
-session = boto3.Session(profile_name='PythonAuto')
+
+session = boto3.Session(profile_name='profile_name_here')
 iam = session.resource('iam') 
 
 
-=======
-session = boto3.Session(profile_name='add-your-username-here')
-iam = session.client('iam') 
->>>>>>> 9ae853e7b62ebbd700d1863091163fb52dff213b
 
 
 print(session.region_name)
