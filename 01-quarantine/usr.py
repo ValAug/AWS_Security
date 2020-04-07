79a6c1949c1380ec29ff578494e90107b177f10b
ValAug <valdiviaaugusto9@gmail.com>
2020-04-06 21:08:17 -0400

    Cleaned up the comments

--- 01-quarantine/usr.py
+++ 01-quarantine/usr.py
@@ -34,11 +34,3 @@ class IAMManager:
         
         return iam_user
 
-# for user in iam.list_users()['Users']:
-#     print("User: {0}\nUserID: {1}\nARN: {2}\nCreatedOn: {3}\n".format(
-#     user['UserName'],
-#     user['UserId'],
-#     user['Arn'],
-#     user['CreateDate']
-#     )
-#     )   
