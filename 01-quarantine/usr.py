a68ba54dddf48ef0492bf9b73eb4d39db061e037
ValAug <valdiviaaugusto9@gmail.com>
2020-04-11 17:04:36 -0400

    Added iam_policy to IAMManager Class

--- 01-quarantine/usr.py
+++ 01-quarantine/usr.py
@@ -6,6 +6,7 @@ import botostubs
 from botocore.exceptions import ClientError
 import click
 
+
 # iam = boto3.client('iam')
 
 class IAMManager:
@@ -25,6 +26,8 @@ class IAMManager:
         """Create new IAM user(Entity), or Return an Entity Already Exists"""
 
         iam_user = None
+        active_user = None
+        
 
         try:
 
@@ -34,6 +37,15 @@ class IAMManager:
         except ClientError as error:
             print(error)
             raise click.Abort()
+        
+        return iam_user
+
+    def iam_policy(self, user_name):
+        """Set IAM policy to be readable by everyone"""
+        return self.iam.attach_user_policy(
+            UserName = user_name,PolicyArn='arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess')
+
+        return
 
         # except ClientError as error:
         #     if error.response["Error"]["Code"] == "EntityAlreadyExists":
@@ -41,5 +53,5 @@ class IAMManager:
         #     else:
         #         raise
         
-        return iam_user
+        
 
