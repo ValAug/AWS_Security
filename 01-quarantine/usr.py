ec4cebbb34ce66bba6c0628075fa7a12cf510132
ValAug <valdiviaaugusto9@gmail.com>
2020-04-13 17:36:36 -0400

    Added more features to usr.py

--- 01-quarantine/usr.py
+++ 01-quarantine/usr.py
@@ -14,7 +14,8 @@ class IAMManager:
 
     def __init__(self, session):
         self.session = session
-        self.iam = self.session.client('iam')
+        self.iam = self.session.client('iam') # type: botostubs.IAM
+    
 
         self.manifest = {}
 
@@ -22,30 +23,64 @@ class IAMManager:
         """Get IAM users list """
         return self.iam.list_users()
 
-    def init_user(self, user_name):
+    def init_user(self, user_name, access_key):
         """Create new IAM user(Entity), or Return an Entity Already Exists"""
 
         iam_user = None
         active_user = None
         
-
         try:
 
             return self.iam.create_user(UserName=user_name)
-            # iam_user = self.iam.create_user(UserName=user_name)
-        
+
         except ClientError as error:
             print(error)
             raise click.Abort()
         
         return iam_user
 
+    # Create an access key
+    def new_keys(self, user_name):
+        """Create new Access Keys"""
+        try:
+            response = self.iam.create_access_key(
+             UserName = user_name) 
+            AccessKey = response['AccessKey']['AccessKeyId']
+            SecretAccessKey = response['AccessKey']['SecretAccessKey']
+            return AccessKey, SecretAccessKey
+
+        except ClientError as error:
+            print(error)
+            raise click.Abort()
+
+    def list_acckeys(self, user_name):
+        """List IAM user Access Keys"""
+        response = self.iam.list_access_keys(UserName=user_name)
+        print(response)
+        
+    def deactivate_keys(self, key, user_name):
+        self.iam.update_access_key(AccessKeyId= key,
+        Status='Inactive',
+        UserName= user_name)
+
+    def delete_acckeys(self, key, user_name):
+        """Delete IAM user Access Keys"""
+        response = self.iam.delete_access_key(AccessKeyId= key,UserName=user_name)
+        print(response)
+
     def iam_policy(self, user_name):
-        """Set IAM policy to be readable by everyone"""
+        """Set an IAM policy to be readable by everyone"""
         return self.iam.attach_user_policy(
             UserName = user_name,PolicyArn='arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess')
 
         return
+
+    def user_detail(self):
+        """List users Policy names and ARNs"""
+        for user_detail in self.iam.get_account_authorization_details(Filter=['User'])['UserDetailList']:policyname = [] 
+        policyarn = []
+        
+        print(user_detail, '\n')
 
         # except ClientError as error:
         #     if error.response["Error"]["Code"] == "EntityAlreadyExists":
